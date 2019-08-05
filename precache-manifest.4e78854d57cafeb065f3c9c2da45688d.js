cdf905ab1e9c688fa701c798d0eb56d7795df9f8
Ivan Poberezhnyuk <vanyapoberejnuk@gmail.com>
2019-08-05 22:38:17 +0300

    Updates

---
+++ precache-manifest.4e78854d57cafeb065f3c9c2da45688d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2a7b11164301cfc3f8ca54c86bfea2",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "10fd7a7fd140c191cab1",
+    "url": "/react_pagination/static/css/main.26f9a00d.chunk.css"
+  },
+  {
+    "revision": "9bc221c26cf05ba9cc03",
+    "url": "/react_pagination/static/js/2.93226d7a.chunk.js"
+  },
+  {
+    "revision": "10fd7a7fd140c191cab1",
+    "url": "/react_pagination/static/js/main.e7ca5cd9.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
